fa400ed85361f39ac756b1ae336ca4984c581db2
cq.longhaibin <haibin.long@edoc2.com>
2017-05-08 19:25:58 +0800

    加入sass
    Signed-off-by: cq.longhaibin <haibin.long@edoc2.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,23 @@
+var path = require('path');
 var connect = require('gulp-connect');
 var gulp = require('gulp');
+var compass = require('gulp-compass');
 
 gulp.task('connect',function(){
     connect.server({
         root: './',
         port: 9001
     })
+});
+
+//编译sass
+gulp.task('compass',function(){
+    var skinName = 'default';
+    gulp.src('./assets/'+ skinName +'/*.scss')
+    .pipe(compass({
+        project: path.join(__dirname, 'assets/' + skinName),
+        css: 'css',
+        sass: 'sass'
+    }))
+    .pipe(gulp.dest('./skin'));
 });
